6942fb343cd80006dbc91f391f0e8d3bd39a5470
Vendicated <vendicated@riseup.net>
2021-06-07 22:59:22 +0200

    Add adb connect to script

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,6 +12,10 @@ cd ../buildtool
 ./buildtool -p "$1"
 
 cd ../buildsPlugins
+# connect to device via adb over network if not connected already
+[ "$(adb devices | wc -l)" = "2" ] && adb connect 192.168.178.21:5555
+
 adb push "${1}.zip" /storage/emulated/0/Aliucord/plugins
 adb shell am force-stop com.aliucord
+# launch Aliucord
 adb shell monkey -p com.aliucord -c android.intent.category.LAUNCHER 1
