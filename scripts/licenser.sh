f0311307b2baaa2d122dac3b2d0cbcba879cc518
Vendicated <vendicated@riseup.net>
2021-06-08 18:31:21 +0200

    Improve licenser script

--- scripts/licenser.sh
+++ scripts/licenser.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 shopt -s globstar
 
-for file in **/*.java
+for file in $(git diff --staged --diff-filter=ACMR --name-only | grep -E ".java$")
 do
   if [ -f "$file" ] && ! grep -q Copyright "$file"
   then
     cat scripts/licenseHeader.txt "$file" > "$file.licensed" && mv "$file.licensed" "$file"
     git add "$file"
+    echo "Added license header to $file"
+  else
+    echo "Skipped $file as it already had a license header"
   fi
 done
