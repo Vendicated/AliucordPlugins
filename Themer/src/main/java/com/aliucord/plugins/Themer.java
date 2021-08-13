5e8d55f456f3c859751f3818e7d0fbf1897d3461
Vendicated <vendicated@riseup.net>
2021-08-13 05:04:30 +0200

    Let's release themer!

--- Themer/src/main/java/com/aliucord/plugins/Themer.java
+++ Themer/src/main/java/com/aliucord/plugins/Themer.java
@@ -63,7 +63,7 @@ public class Themer extends Plugin {
                 new Manifest.Author("AAGaming", 373833473091436546L),
         };
         manifest.description = "Adds support for custom themes & fonts";
-        manifest.version = "1.0.0";
+        manifest.version = "1.0.1";
         manifest.updateUrl = "https://raw.githubusercontent.com/Vendicated/AliucordPlugins/builds/updater.json";
         return manifest;
     }
