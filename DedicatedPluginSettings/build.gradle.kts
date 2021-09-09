e5c0aa8ae988012b92e4768250a9768480b08f7b
Vendicated <vendicated@riseup.net>
2021-09-09 16:56:55 +0200

    DedicatedPluginSettings: Improve implementation

--- DedicatedPluginSettings/build.gradle.kts
+++ DedicatedPluginSettings/build.gradle.kts
@@ -1,2 +1,9 @@
-version = "1.0.3"
+version = "1.0.4"
 description = "Adds a dedicated plugin settings category to the settings page"
+
+aliucord.changelog.set(
+    """
+        # 1.0.4
+        * Improve implementation
+    """.trimIndent()
+)
---
+++ DedicatedPluginSettings/src/main/java/dev/vendicated/aliucordplugs/dps/PluginsAdapter.kt
@@ -0,0 +1,135 @@
+/*
+ * Ven's Aliucord Plugins
+ * Copyright (C) 2021 Vendicated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+package dev.vendicated.aliucordplugs.dps
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.RecyclerView
+import com.aliucord.*
+import com.aliucord.entities.Plugin
+import com.aliucord.utils.ReflectUtils
+import com.discord.utilities.color.ColorCompat
+import com.lytefast.flexinput.R
+import com.lytefast.flexinput.R.d.*
+
+private operator fun Int.not() =
+    ContextCompat.getDrawable(Utils.appContext, this) ?: ContextCompat.getDrawable(Utils.appContext, ic_slash_command_24dp)!!.also {
+        Logger("DedicatedPluginSettings")
+            .warn(
+                "No such drawable: $this (${
+                    try {
+                        Utils.appActivity.resources.getResourceName(this)
+                    } catch (th: Throwable) {
+                        "Unknown Resource"
+                    }
+                })"
+            )
+    }
+
+private var drawables = hashMapOf(
+    "fallback" to !ic_slash_command_24dp,
+    // Ven
+    "TapTap" to !ic_raised_hand_action_24dp,
+    "Themer" to !ic_theme_24dp,
+    "Hastebin" to !ic_link_white_24dp,
+    "ImageUploader" to !ic_uploads_image_dark,
+    "EmojiUtility" to !ic_emoji_24dp,
+    // Juby
+    "Experiments" to !ic_security_24dp,
+    "UserDetails" to !ic_my_account_24dp,
+    "PronounDB" to !ic_accessibility_24dp,
+    "CustomTimestamps" to !ic_clock_black_24dp,
+    "CustomNicknameFormat" to !ic_account_circle_white_24dp,
+    "RemoveZoomLimit" to !ic_search_white_24dp,
+    // Moth
+    "RotatedChat" to !com.yalantis.ucrop.R.c.ucrop_rotate, // This is from https://github.com/Yalantis/uCrop lmao
+    "RoleColorEverywhere" to !ic_theme_24dp,
+    // Xinto
+    "LayoutController" to !design_ic_visibility_off,
+    // Wing
+    "GuildProfiles" to !ic_members_24dp,
+    "CustomBadges" to !ic_staff_badge_blurple_24dp
+)
+
+class PluginsAdapter(private val fm: FragmentManager) : RecyclerView.Adapter<ViewHolder>() {
+    val data = PluginManager.plugins.values.filter {
+        PluginManager.isPluginEnabled(it.getName()) && it.settingsTab != null
+    }.sortedBy {
+        it.getName()
+    }.toMutableList()
+
+    override fun getItemCount() = data.size.coerceAtLeast(1)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(
+            this,
+            TextView(parent.context, null, 0, R.h.UiKit_Settings_Item_Icon)
+        )
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView as TextView
+
+        if (data.isEmpty()) {
+            holder.itemView.text = "Hmm... No Settings"
+        } else {
+            val p = data[position]
+            val name = p.getName()
+            holder.itemView.text = name
+
+            val drawable = drawables[name] ?: try {
+                ReflectUtils.getField(p, "pluginIcon") as Drawable
+            } catch (th: Throwable) {
+                null
+            } ?: drawables["fallback"]
+
+            holder.itemView.setCompoundDrawablesRelativeWithIntrinsicBounds(drawable?.apply {
+                mutate()
+                setTint(ColorCompat.getThemedColor(holder.itemView.context, R.b.colorInteractiveNormal))
+            }, null, null, null)
+        }
+    }
+
+    fun onEntryClicked(ctx: Context, position: Int) {
+        val p = data[position]
+        try {
+            if (p.settingsTab.type == Plugin.SettingsTab.Type.PAGE && p.settingsTab.page != null) {
+                val page = ReflectUtils.invokeConstructorWithArgs(p.settingsTab.page, *(p.settingsTab.args ?: emptyArray()))
+                Utils.openPageWithProxy(ctx, page)
+            } else if (p.settingsTab.type == Plugin.SettingsTab.Type.BOTTOM_SHEET && p.settingsTab.bottomSheet != null) {
+                ReflectUtils.invokeConstructorWithArgs(p.settingsTab.bottomSheet, *(p.settingsTab.args ?: emptyArray()))
+                    .show(fm, p.getName() + "Settings")
+            }
+        } catch (th: Throwable) {
+            PluginManager.logger.error(ctx, "Failed to launch plugin settings", th)
+        }
+    }
+}
+
+class ViewHolder(private val adapter: PluginsAdapter, itemView: TextView) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    init {
+        itemView.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+        itemView.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        adapter.onEntryClicked(v.context, adapterPosition)
+    }
+}
