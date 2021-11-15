c5d88e42d4c17645f69086d49310598d177246ca
Ven <vendicated@riseup.net>
2021-11-15 02:07:27 +0100

    Update build.gradle.kts

--- TapTap/build.gradle.kts
+++ TapTap/build.gradle.kts
@@ -5,5 +5,8 @@ aliucord.changelog.set(
     """
         # 1.1.3
         * Select the input box if opening keyboard
+        
+        # 1.1.0
+        * Automatically open keyboard when replying to a message. Disabled by default, toggleable via settings
     """.trimIndent()
 )
