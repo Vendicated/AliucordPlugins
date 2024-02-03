344bea16ddf5ef6d1e8ae828001dc9829eb12486
V <vendicated@riseup.net>
2024-02-03 18:21:41 +0100

    Update build.gradle.kts

--- TextFilePreview/build.gradle.kts
+++ TextFilePreview/build.gradle.kts
@@ -1,8 +1,11 @@
-version = "1.0.2"
+version = "1.0.3"
 description = "Preview the content of Text files in Discord"
 
 aliucord.changelog.set(
     """
+        # 1.0.3
+        * Fix probably idk i didnt test
+
         # 1.0.1
         * Reduce default preview size to 300 and add a setting for it
         * Add close button that will hide the preview
