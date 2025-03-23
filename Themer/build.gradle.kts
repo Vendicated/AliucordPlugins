ac676935e53f97692335c19083ea165ab9a822d7
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-22 23:18:00 -0400

    Update build.gradle.kts

--- Themer/build.gradle.kts
+++ Themer/build.gradle.kts
@@ -3,19 +3,19 @@ description = "Apply custom themes to your Discord"
 
 aliucord.changelog.set(
     """
-  # 3.6.5
-  *  added file garden to allowed links
+    # 3.6.5
+    * added file garden to allowed links
 
-   # 3.6.4
-   *  added catbox to allowed links
+    # 3.6.4
+    * added catbox to allowed links
 
-   # 3.6.3
-   *  got rid of discord links being in the supported links
+    # 3.6.3
+    * got rid of discord links being in the supported links
 
     # 3.6.2
     * Now prompts to switch to dark mode if using light/pureEvil theme
     
     # 3.6.1
     * Re-enabled custom fonts. They may still be unstable, so use at your own risk  
-""".trimIndent()
+    """.trimIndent()
 )
