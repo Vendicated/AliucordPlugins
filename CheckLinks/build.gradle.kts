eba69bf6c8cbfc0672c437dbded1ee6b9d6ada75
veeaces96@gmail.com <131408227+OasisVee@users.noreply.github.com>
2025-03-22 22:08:36 -0400

    updated

--- CheckLinks/build.gradle.kts
+++ CheckLinks/build.gradle.kts
@@ -1,6 +1,8 @@
-version = "1.0.8"
+version = "1.0.9"
 description = "Checks links via the VirusTotal api"
 
 aliucord.changelog.set("""
-    Fix
+    # 1.0.9
+    * added an api key setting to the settings page for the user to input their own api key
+    * updated the api url to v3
 """.trimIndent())
