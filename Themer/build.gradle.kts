a7dedc52318510a80b0caa1bc93cf7514452f941
LavaDesu <me@lava.moe>
2025-08-21 02:46:08 +1000

    Themer: bump version

--- Themer/build.gradle.kts
+++ Themer/build.gradle.kts
@@ -1,8 +1,11 @@
-version = "3.6.3"
+version = "3.6.4"
 description = "Apply custom themes to your Discord"
 
 aliucord.changelog.set(
     """
+    # 3.6.4
+    * Fix color theming for some elements like user profile header and chatbox.
+
     # 3.6.3
     * Fix color theming for newer android versions
 
